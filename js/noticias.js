5f8cc1bc2192a6e1e5a71f22d86634247fe2778b
Dario Rodriguez <darrodpaj@gmail.com>
2025-01-04 21:46:47 +0100

    .

--- js/noticias.js
+++ js/noticias.js
@@ -1,14 +1,15 @@
 function abrirNoticia(selector) {
-    $(selector).fadeIn(300); // Muestra con un efecto de desvanecimiento
+    $(selector).fadeIn(300);
 }
 
 function cerrarNoticia(selector) {
-    $(selector).fadeOut(300); // Oculta con un efecto de desvanecimiento
+    $(selector).fadeOut(300);
 }
 
+// Ocultar noticia con el boton escape
 $(document).on('keydown', function (e) {
     if (e.key === "Escape") {
-        $('.mas-noticia:visible').fadeOut(300); // Oculta cualquier noticia visible
+        $('.mas-noticia:visible').fadeOut(300);
     }
 });
 
